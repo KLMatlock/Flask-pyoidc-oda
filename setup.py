1a3fc79f1a478307317046665545f91d3839510f
klmatlock <kevin.matlock@omicsautomation.com>
2021-03-29 13:44:52 -0700

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='Flask-pyoidc-oda',
-    version='0.2.1',
+    version='0.3.0',
     packages=['flask_pyoidc'],
     package_dir={'': 'src'},
     url='https://github.com/zamzterz/flask-pyoidc',
--- src/flask_pyoidc/__init__.py
+++ src/flask_pyoidc/__init__.py
@@ -1,3 +1,3 @@
 from .flask_pyoidc import OIDCAuthentication, NoAuthenticationError
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
