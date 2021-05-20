49d279014019bd43822130ccacc52182e0229559
klmatlock <kevin.matlock@omicsautomation.com>
2021-05-19 21:16:20 -0700

    Bumpted version to 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='Flask-pyoidc-oda',
-    version='0.4.1',
+    version='0.5.0',
     packages=['flask_pyoidc'],
     package_dir={'': 'src'},
     url='https://github.com/zamzterz/flask-pyoidc',
